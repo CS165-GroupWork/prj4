05faa77d76b52707ae21d7995f4fa5fc1bf4df38
Drey Platt <dreyplatt@gmail.com>
2016-02-13 20:08:27 -0700

    Initial Commit

---
+++ Asteroids_CS165/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Asteroids_CS165
+//
+//  Created by Drey Platt on 2/13/16.
+//  Copyright (c) 2016 Drey Platt. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
